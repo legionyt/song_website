7506dbe570206f1fd3ce08e54f45986f95a7bfa9
dushy <legionyt813@gmail.com>
2022-01-04 11:56:32 +0530

    source code

---
+++ server.py
@@ -0,0 +1,35 @@
+import flask
+import json
+
+app = flask.Flask(__name__)
+
+with open('data.json') as f:
+    data = json.loads(f.read())
+
+
+@app.route('/', methods=['GET'])
+def choose_lang():
+    print(list(data.keys()))
+    length = len(list(data.keys()))
+    languages = list(data.keys())
+    return flask.render_template('choose_lang.html', data=data)
+
+
+@app.route('/choose_genre')
+def return_genre_page():
+    lang = flask.request.args.get('language')
+    genres = data[lang]
+    print(genres)
+    return flask.render_template('choose genre.html', genres=genres, lang=lang)
+
+
+@app.route('/choose_song')
+def return_song_page():
+    genre = flask.request.args.get('genre')
+    language = flask.request.args.get('language')
+    songs = data[language][genre]
+    print(songs)
+    return flask.render_template('choose_song.html', songs=songs)
+
+
+app.run('0.0.0.0', port=8080)
